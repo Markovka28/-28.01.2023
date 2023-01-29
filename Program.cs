ef5c51a6bbfce538d14ba60ece1fa9349179157d
Valery_Krylov <supermaaaan@mail.ru>
2023-01-29 16:25:41 +0500

    решение задачи 60

--- Program.cs
+++ Program.cs
@@ -1,91 +1,126 @@
-﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение 
-двух матриц.*/
-using System;
+﻿/*Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите 
+программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
-// класс с методами расширения
-static class MatrixExt
+Console.Write("Введите размеры массива через пробел: ");
+string[] num = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] matrix = GetArray(new int[] {int.Parse(num[0]), int.Parse(num[1]), int.Parse(num[2])}, 10,99);
+PrintArray(matrix); 
+int[,,] GetArray(int[] index, int min, int max)
 {
-    // получение строк матрицы
-    public static int RowsCount(this int[,] matrix)
+    int[,,] result = new int [index[0], index[1], index[2]];
+    for (int i = 0; i < result.GetLength(0); i++)
     {
-        return matrix.GetUpperBound(0) + 1;
-    }
-    // метод получение столбцов матрицы
-    public static int ColumnsCount(this int[,] matrix)
-    {
-        return matrix.GetUpperBound(1) + 1;
-    }   
-}
-class Program
-{
-    // метод получения матрицы из консоли
-    static int[,] GetMatrixFromConsole(string name)
-    {
-        Console.Write("Количество строк матрицы {0}:    ", name);
-        var n = int.Parse(Console.ReadLine());
-        Console.Write("Количество столбцов матрицы {0}: ", name);
-        var m = int.Parse(Console.ReadLine());
-        var matrix = new int[n, m];
-        for (var i = 0; i < n; i++)
+        for (int j = 0; j < result.GetLength(1); j++)
         {
-            for (var j = 0; j < m; j++)
+            for (int k = 0; k < result.GetLength(2); k++)
             {
-                for (int k = 0; k < matrix.GetLength(1); k++)
-                matrix[i, k] = new Random().Next(1, 9);
-            }
-        }
-        return matrix;
-    }
-    // метод для печати матрицы в консоль
-    static void PrintMatrix(int[,] matrix)
-    {
-        for (var i = 0; i < matrix.RowsCount(); i++)
-        {
-            for (var j = 0; j < matrix.ColumnsCount(); j++)
-            {
-                Console.Write(matrix[i, j].ToString().PadLeft(4));
-            }
-            Console.WriteLine();
-        }
-    }
-    // метод умножения матриц
-    static int[,] MatrixMultiplication(int[,] MatrixI, int[,] MatrixII)
-    {       
-        if (MatrixI.ColumnsCount() != MatrixII.RowsCount())
-        {
-            throw new Exception("Количество столбцов матрицы 1 не равно количеству строк матрицы 2, произведение невыполнимо");
-        }
-        var ResultMatrix = new int[MatrixI.RowsCount(), MatrixII.ColumnsCount()];
-
-        for (var i = 0; i < MatrixI.RowsCount(); i++)
-        {
-            for (var j = 0; j < MatrixII.ColumnsCount(); j++)
-            {
-                ResultMatrix[i, j] = 0;
-
-                for (var k = 0; k < MatrixI.ColumnsCount(); k++)
+                int element = new Random().Next(min, max +1);
+                if (FindElement(result, element)) continue;
                 {
-                    ResultMatrix[i, j] += MatrixI[i, k] * MatrixII[k, j];
+                    result[i,j,k] = element;
+                    k++;
                 }
             }
         }
-        return ResultMatrix;
     }
-    static void Main(string[] args)
-    {    
-        Console.WriteLine("Умножение матриц");
-
-        var a = GetMatrixFromConsole("1");
-        var b = GetMatrixFromConsole("2");
-
-        Console.WriteLine("Матрица 1:");
-        PrintMatrix(a);
-
-        Console.WriteLine("Матрица 2:");
-        PrintMatrix(b);
-
-        var result = MatrixMultiplication(a, b);
-        Console.WriteLine("Произведение матриц:");
-        PrintMatrix(result);
+    return result;
+int Findelement (int[,,] matrix, int el);
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (matrix[i,j,k]== el) return true;
+            }
+        }
     }
+    return false;
+}
+void PrintArray(int[,,] matrix)
+{    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i,j,k]} ({i} {j} {k})   ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+} этот код разбирали с преподавателем но он не работает в чем там ошибка я не разобрался код ниже использовал по примеру из интернета*/
+
+Console.WriteLine($"Введите размер массива X x Y x Z: ");
+int a = Mass3D("Введите длину строки: ");
+int b = Mass3D("Введите высоту столбцов: ");
+int c = Mass3D("Введите глубину массива: ");
+Console.WriteLine($" ");
+
+int[,,] matrix = new int[a, b, c];
+GetArray(matrix);
+PrintArray(matrix);
+
+int Mass3D(string num)
+{
+  Console.Write(num);
+  int element = Convert.ToInt32(Console.ReadLine());
+  return element;
+}
+
+void PrintArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void GetArray(int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 99);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 99);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
 }
